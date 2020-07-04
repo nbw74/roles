b53144a650a547665efd9c2e9c2cd3c95634fdf2
Nbw <nbw.adm@gmail.com>
2020-07-04 22:41:31 +0400

    Add --local feature in kvm/files/vmbackup.sh

--- kvm/files/vmbackup.sh
+++ kvm/files/vmbackup.sh
@@ -10,10 +10,10 @@ set -o pipefail
 # set -xv
 
 # DEFAULTS BEGIN
-typeset -i DEBUG=0 IGNORE_POWEROFF=0 SHUTDOWN=0
+typeset -i DEBUG=0 IGNORE_POWEROFF=0 SHUTDOWN=0 LOCAL=0
 typeset -i BACKUP_DEPTH=3 SNAPSHOT_SIZE=10
 typeset REMOTE_USER=$(whoami)
-typeset REMOTE_HOST="" REMOTE_BASEDIR=""
+typeset REMOTE_HOST="" BASEDIR=""
 # DEFAULTS END
 
 # CONSTANTS BEGIN
@@ -39,16 +39,18 @@ main() {
 
     checks "${1:-nop}"
 
-    local connect="ssh -o PasswordAuthentication=no ${REMOTE_USER}@$REMOTE_HOST"
+    if (( ! LOCAL )); then
+	local connect="ssh -o PasswordAuthentication=no ${REMOTE_USER}@$REMOTE_HOST"
+    fi
 
     while (( $# )); do
 
-	local remote_dir="" domain="" device=""
+	local backup_dir="" domain="" device=""
 	local -i blk_count=0
 
 	domain="$1"
-	remote_dir="${REMOTE_BASEDIR}/$domain"
-	(( DEBUG )) && echo "* DEBUG: domain=$domain remote_dir=$remote_dir"
+	backup_dir="${BASEDIR}/$domain"
+	(( DEBUG )) && echo "* DEBUG: domain=$domain backup_dir=$backup_dir"
 
 	removeOld
 	# Get first block device
@@ -80,12 +82,21 @@ main() {
 removeOld() {
     local fn=${FUNCNAME[0]}
 
-    if $connect "test -d '$remote_dir'"; then
-	logInfo "pruning old backups"
-	# shellcheck disable=SC1117
-	$connect "cd '$remote_dir' && find . -maxdepth 1 -mindepth 1 -type f -printf '%P\n' | sort -r | tail -n +$BACKUP_DEPTH | xargs -r rm --"
+    if (( LOCAL )); then
+	if [[ -d "$backup_dir" ]]; then
+	    logInfo "pruning old backups"
+	    cd "$backup_dir" || false
+	    find . -maxdepth 1 -mindepth 1 -type f -printf '%P\n' | sort -r | tail -n +$BACKUP_DEPTH | xargs -r rm --
+	else
+	    mkdir "$backup_dir"
+	fi
     else
-	$connect "mkdir '$remote_dir'"
+	if $connect "test -d '$backup_dir'"; then
+	    logInfo "pruning old backups"
+	    $connect "cd '$backup_dir' && find . -maxdepth 1 -mindepth 1 -type f -printf '%P\n' | sort -r | tail -n +$BACKUP_DEPTH | xargs -r rm --"
+	else
+	    $connect "mkdir '$backup_dir'"
+	fi
     fi
 }
 
@@ -114,7 +125,7 @@ backupShutdown() {
     done
     (( DEBUG )) && echo
 
-    remoteDump "$device"
+    makeDump "$device"
 
     logInfo "domain starting"
     virsh -q start "$domain"
@@ -154,20 +165,24 @@ backupSnapshot() {
     logInfo "thawing filesystem"
     virsh -q domfsthaw "$domain"
 
-    remoteDump "$snapshot"
+    makeDump "$snapshot"
 
     snapshotted=0
     logInfo "removing snapshot"
     lvremove -qqf "$snapshot"
 }
 
-remoteDump() {
+makeDump() {
     local fn=${FUNCNAME[0]}
     local dev=$1
 
-    logInfo "dump to remote server"
-    # shellcheck disable=SC2216
-    ionice -c3 dd if="$dev" bs=8M | nice lbzip2 - | $connect "dd of=${remote_dir}/$(date +'%s')-${domain}.img.bz2 bs=8M"
+    if (( LOCAL )); then
+	logInfo "dump to local server"
+	ionice -c3 dd if="$dev" bs=8M | nice lbzip2 - | dd of="${backup_dir}/$(date +'%s')-${domain}.img.bz2" bs=8M
+    else
+	logInfo "dump to remote server"
+	ionice -c3 dd if="$dev" bs=8M | nice lbzip2 - | $connect "dd of=${backup_dir}/$(date +'%s')-${domain}.img.bz2 bs=8M"
+    fi
 }
 
 logInfo() {
@@ -192,7 +207,12 @@ checks() {
 	false
     fi
 
-    if [[ "$1" == "nop" || -z $REMOTE_BASEDIR || -z $REMOTE_HOST ]]; then
+    if [[ "$1" == "nop" || ${BASEDIR:-nop} == "nop" ]]; then
+	echo "required parameter missing" >&2
+	false
+    fi
+
+    if [[ $LOCAL == 0 && ${REMOTE_HOST:-nop} == "nop" ]]; then
 	echo "required parameter missing" >&2
 	false
     fi
@@ -234,11 +254,12 @@ usage() {
     Usage: $bn [OPTIONS] <parameter>\\n
     Options:
 
-    -b, --basedir <path>	base directory for backups on remote host
+    -b, --basedir <path>	base directory for backups on remote or local host
     -D, --depth <int>		backup depth (default is ${BACKUP_DEPTH})
     -H, --host <string>		remote host
     -U, --user <strng>		remote user
     -i, --ignore-poweroff	don't error if host is already powered off
+    -l, --local			do local backup instead of remote
     -s, --shutdown		shut down VM before backup (instead of making snapshot)
     -S, --snapshot-size <int>	size of snapshot, GB (default is ${SNAPSHOT_SIZE})
     -d, --debug			print some info in stdout/stderr
@@ -248,7 +269,7 @@ usage() {
 # Getopts
 getopt -T; (( $? == 4 )) || { echo "incompatible getopt version" >&2; exit 4; }
 
-if ! TEMP=$(getopt -o b:D:H:S:U:isdh --longoptions basedir:,depth:,host:,user:,ignore-poweroff,shutdown,snapshot-size:,debug,help -n "$bn" -- "$@")
+if ! TEMP=$(getopt -o b:D:H:S:U:ilsdh --longoptions basedir:,depth:,host:,user:,ignore-poweroff,local,shutdown,snapshot-size:,debug,help -n "$bn" -- "$@")
 then
     echo "Terminating..." >&2
     exit 1
@@ -259,11 +280,12 @@ unset TEMP
 
 while true; do
     case $1 in
-	-b|--basedir)		REMOTE_BASEDIR=$2 ;	shift 2	;;
+	-b|--basedir)		BASEDIR=$2 ;		shift 2	;;
 	-D|--depth)		BACKUP_DEPTH=$2 ;	shift 2	;;
 	-H|--host)		REMOTE_HOST=$2 ;	shift 2	;;
 	-U|--user)		REMOTE_USER=$2 ;	shift 2	;;
 	-i|--ignore-poweroff)	IGNORE_POWEROFF=1 ;	shift	;;
+	-l|--local)		LOCAL=1 ;		shift	;;
 	-s|--shutdown)		SHUTDOWN=1 ;		shift	;;
 	-S|--snapshot-size)	SNAPSHOT_SIZE=$2 ;	shift 2	;;
 	-d|--debug)		DEBUG=1 ;		shift	;;
